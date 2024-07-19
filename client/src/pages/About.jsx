649d2eb9140a56c7e7e237090364fceff7d9fe61
Yuvraj Singh Chowdhary <chowdharyyuvrajsingh@gmail.com>
2024-07-19 16:52:18 +0530

    initial commit

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Cipher Villa</h1>
+      <p className='mb-4 text-slate-700'>Cipher Villa is a premier real estate agency renowned for facilitating the buying, selling, and renting of properties in the most coveted neighborhoods. With a team of seasoned agents, we are committed to delivering unparalleled service, ensuring a seamless and stress-free experience for our clients. Our expertise and dedication have helped countless clients find their dream homes and achieve optimal returns on their investments, setting us apart as industry leaders.</p>
+      <p className='mb-4 text-slate-700'>
+      Our mission at Cipher Villa is to help clients achieve their real estate goals by offering expert advice, personalized service, and a deep understanding of the local market. Whether you're looking to buy, sell, or rent a property, our dedicated team is here to assist you every step of the way. 🏡✨ With our commitment to excellence and client satisfaction, we make your real estate journey smooth and successful. 🌟📈
+      </p>
+      <p className='mb-4 text-slate-700'>Our team of agents possesses a wealth of experience and knowledge in the real estate industry. We are committed to providing the highest level of service to our clients. 🏆🏠 We believe that buying or selling a property should be an exciting and rewarding experience, and we are dedicated to making that a reality for each of our clients. 🌟✨ Let us help you navigate the real estate market with confidence and ease.</p>
+    </div>
+  )
+}
